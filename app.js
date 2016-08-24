3a3ddd26b0071728a8c74e87c634c77b16c80f42
Jake Willingham <jakewillingham789@gmail.com>
2016-08-24 10:50:11 -0400

    changed settings

--- app.js
+++ app.js
@@ -13,12 +13,13 @@ var multerFileUpload = multerFormInput({ dest: 'public/images/' })
 var app = expressWebServer()
 
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+    next();
+}
 
 // Routes
 app.get('/api/v1/portfolio', function(req, res){
@@ -42,6 +43,15 @@ app.post('/save', multerFileUpload.single('image'), function (req, res) {
 })
 
 // Start
+app.configure(function() {
+    app.use(expressWebServer.bodyParser());
+    app.use(expressWebServer.cookieParser());
+    app.use(expressWebServer.session({ secret: 'cool beans' }));
+    app.use(expressWebServer.methodOverride());
+    app.use(allowCrossDomain);
+    app.use(app.router);
+    app.use(expressWebServer.static('public'))
+})
 // app.use(expressWebServer.static('public'))
 app.listen(process.env.PORT || port, function () {
   console.log('Web server on http://localhost:' + port)
