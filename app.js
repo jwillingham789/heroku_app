fa586940d63fc5c22701b71a9d2217d15a6fd269
Jake Willingham <jakewillingham789@gmail.com>
2016-08-24 21:17:47 -0400

    data now on heroku

--- app.js
+++ app.js
@@ -13,33 +13,16 @@ var multerFileUpload = multerFormInput({ dest: 'public/images/' })
 var cors = require('cors')
 var app = expressWebServer()
 
-// var allowCrossDomain = function(req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-//
-//     // intercept OPTIONS method
-//     if ('OPTIONS' == req.method) {
-//       res.send(200);
-//     }
-//     else {
-//       next();
-//     }
-// };
-//
-// app.use(allowCrossDomain);
-
 app.use(cors())
 
 // Routes
 app.get('/api/v1/portfolio', function(req, res){
-  res.json({x: true})
-  // knexDatabase
-  //   .select()
-  //   .from('portfolio')
-  //   .then(function(data){
-  //     res.json(data)
-  //   })
+  knexDatabase
+    .select()
+    .from('portfolio')
+    .then(function(data){
+      res.json(data)
+    })
 })
 
 app.post('/save', multerFileUpload.single('image'), function (req, res) {
--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 module.exports = {
     client: 'pg',
-    connection: 'postgres://localhost/JakeWillingham'
+    connection: 'postgres://jjmuezotryzrrg:b488dHjW2vS9ETECKf6q8mdFHv@ec2-54-235-95-102.compute-1.amazonaws.com:5432/db3qon2josueam
+'
 }
