5de6046d814585c57c1e1cf3a1acc3175a5b298c
Jake Willingham <jakewillingham789@gmail.com>
2016-08-24 21:24:48 -0400

    made edits

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,4 @@
 module.exports = {
     client: 'pg',
-    connection: 'postgres://jjmuezotryzrrg:b488dHjW2vS9ETECKf6q8mdFHv@ec2-54-235-95-102.compute-1.amazonaws.com:5432/db3qon2josueam
-'
+    connection: 'postgres://jjmuezotryzrrg:b488dHjW2vS9ETECKf6q8mdFHv@ec2-54-235-95-102.compute-1.amazonaws.com:5432/db3qon2josueam'
 }
